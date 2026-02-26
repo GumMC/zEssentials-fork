faa7b1fdf8b5e758c813e371970d7f3185062e37
TuanLA <lat.dev08@gmail.com>
2026-02-26 17:03:56 +0700

    Fix inbox sql

--- API/src/main/java/fr/maxlego08/essentials/api/dto/InboxDTO.java
+++ API/src/main/java/fr/maxlego08/essentials/api/dto/InboxDTO.java
@@ -4,5 +4,5 @@ import java.util.Date;
 import java.util.UUID;
 
 public record InboxDTO(int id, UUID receiver_id, UUID sender_id, String sender_name, String message, boolean is_read,
-                       Date expire_at, Date created_at) {
+                       Date expire_at, Date created_at, Date updated_at) {
 }
--- src/main/java/fr/maxlego08/essentials/migrations/create/CreateInboxTableMigration.java
+++ src/main/java/fr/maxlego08/essentials/migrations/create/CreateInboxTableMigration.java
@@ -8,11 +8,11 @@ public class CreateInboxTableMigration extends Migration {
     public void up() {
         create("%prefix%inbox", table -> {
             table.autoIncrement("id");
-            table.uuid("receiver_id").foreignKey("%prefix%users");
+            table.uuid("receiver_id").foreignKey("%prefix%users", "unique_id", true);
             table.uuid("sender_id").nullable();
             table.string("sender_name", 64);
             table.text("message");
-            table.bool("is_read", false);
+            table.bool("is_read").defaultValue(0);
             table.timestamp("expire_at").nullable();
             table.timestamps();
         });
